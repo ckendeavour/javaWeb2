5f26f0eb706b1fde69f1626b0cf0f973abea5237
Administrator <635862336@qq.com>
2020-07-31 14:51:33 +0800

    测试提交

---
+++ src/com/ckhn/bean/User.java
@@ -0,0 +1,42 @@
+package com.ckhn.bean;
+
+/**
+ * @author cuikk
+ * @create 2020-07-30 19:02
+ * @description
+ */
+public class User {
+
+    //
+    private String name;
+    //
+    private int age;
+
+    //
+    private int idCard;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(int idCard) {
+        this.idCard = idCard;
+    }
+}
---
+++ src/com/ckhn/main.java
@@ -0,0 +1,13 @@
+package com.ckhn;
+
+/**
+ * @author cuikk
+ * @create 2020-07-31 9:02
+ * @description
+ */
+public class main {
+
+    public static void main(String[] args) {
+
+    }
+}
